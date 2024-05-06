d6efd65c8a80e3feb351906de517cc40a6145776
urvish <suvagiyaurvish0902@gmail.com>
2024-05-06 14:26:57 +0530

    mart

--- src/componet/tiktak.js
+++ src/componet/tiktak.js
@@ -9,14 +9,14 @@ function Tiktak() {
             document.getElementById("b1").value = "X";
             document.getElementById("b1").disabled = true;
             flag = 0;
-            console.log(flag,"if");
+            console.log(flag, "if");
         } else {
             document.getElementById("b1").value = "O";
             document.getElementById("b1").disabled = true;
             flag = 1;
-            console.log(flag,"else");
+            console.log(flag, "else");
         }
-        console.log(flag,"end");
+        console.log(flag, "end");
     }
     const myfunc = () => {
         var b1, b2, b3, b4, b5, b6, b7, b8, b9;
@@ -458,8 +458,15 @@ function Tiktak() {
     }
 
     const myfunc_2 = () => {
-        // window.location.reload();
-        //  b1 = b2 = b3 = b4 = b5 = b6 = b7 = b8 = b9 = "";
+        document.getElementById("b1").value = "";
+        document.getElementById("b2").value = "";
+        document.getElementById("b3").value = "";
+        document.getElementById("b4").value = "";
+        document.getElementById("b5").value = "";
+        document.getElementById("b6").value = "";
+        document.getElementById("b7").value = "";
+        document.getElementById("b8").value = "";
+        document.getElementById("b9").value = "";
     }
 
     return (
